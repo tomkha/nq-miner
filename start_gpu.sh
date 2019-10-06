a4eb65b028164f7355abf993d026256ca7fac7af
tomkha <mpiskunov@gmail.com>
2019-10-07 00:58:19 +0300

    Fix start script.

--- start_gpu.sh
+++ start_gpu.sh
@@ -2,6 +2,6 @@
 
 echo "------------------------START Miner----------------------"
 export UV_THREADPOOL_SIZE=32
-./nq-miner`
+./nq-miner
 echo "------------------------END Miner----------------------"
 echo "something went wrong or you exited"
