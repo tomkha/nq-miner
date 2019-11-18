1e7a87113777cbcde4fee6f27c8c5af483ace063
tomkha <mpiskunov@gmail.com>
2019-11-18 22:32:39 +0300

    Version 0.99.6

--- hiveos/h-stats.sh
+++ hiveos/h-stats.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-. /hive/miners/custom/$CUSTOM_MINER/h-manifest.conf
+local MINER_API_PORT=3110
 
-local API_URL="http://localhost:3110/api"
+stats_raw=$(curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://localhost:${MINER_API_PORT}/api)
+if [[ $? -ne 0  || -z $stats_raw ]]
+then
+  echo -e "${YELLOW}Failed to read $miner stats from localhost:${MINER_API_PORT}${NOCOLOR}"
+else
+  local temp=$(jq '.temp' <<< $gpu_stats)
+  local fan=$(jq '.fan' <<< $gpu_stats)
 
-stats=$(curl -s "$API_URL" | jq -c --argjson gpu_stats "$gpu_stats" '{ total_khs: (.totalHashrate / 1000), hs: (.hashrates | map(. / 1000)), hs_units: "khs", temp: $gpu_stats.temp, fan: $gpu_stats.fan, uptime, ver: .version, ar: [.shares - .errors, .errors], algo: "argon2d-nim" }')
-khs=$(jq -r '.total_khs' <<< "$stats")
+  [[ $cpu_indexes_array != '[]' ]] && #remove Internal Gpus
+    temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
+    fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
+
+  stats=$(jq -c --argjson temp "$temp" \
+                --argjson fan "$fan" \
+                '{ total_khs: (.totalHashrate / 1000 | floor), hs: (.hashrates | map(. // 0 | floor)), hs_units: "hs", $temp, $fan, uptime, ver: .version[9:], ar: [.shares - .errors, .errors], algo: "argon2d-nim"}' <<< "$stats_raw")
+  khs=$(jq -r '.total_khs' <<< "$stats")
+fi
 
 [[ -z $stats ]] && stats="null"
 [[ -z $khs ]] && khs=0
